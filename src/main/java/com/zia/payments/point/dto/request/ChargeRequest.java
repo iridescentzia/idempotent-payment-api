18d1f5d1550f98675f5709c25faf8fd2cde69807
iridescentzia <ziaunzip@naver.com>
2026-01-30 15:04:55 +0900

    feat:충전 요청/응답 작성

--- src/main/java/com/zia/payments/point/dto/request/ChargeRequest.java
+++ src/main/java/com/zia/payments/point/dto/request/ChargeRequest.java
@@ -1,4 +1,13 @@
 package com.zia.payments.point.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChargeRequest {
+    private Long amount; // 충전 금액
+    private String memo;
 }
