a1ca67ea8890b396915572f3b3769b8d5b155bcd
iridescentzia <ziaunzip@naver.com>
2026-01-30 12:44:00 +0900

    feat:pointwallet 엔티티 작성

--- src/main/java/com/zia/payments/point/domain/PointWallet.java
+++ src/main/java/com/zia/payments/point/domain/PointWallet.java
@@ -1,4 +1,66 @@
 package com.zia.payments.point.domain;
 
+import com.zia.payments.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(
+        name = "point_wallets",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_point_wallets_user_id", columnNames = {"user_id"})
+        }
+)
 public class PointWallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wallet_id")
+    private Long id;
+
+    // 유저당 지갑 1개
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_wallet_user"))
+    private User user;
+
+    @Column(name = "balance", nullable = false)
+    private Long balance;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+        if (this.balance == null) this.balance = 0L;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void increase(long amount) {
+        if(amount <= 0) throw new IllegalArgumentException("금액은 양수여야 합니다.");
+        this.balance += amount;
+    }
+
+    public void decrease(long amount) {
+        if(amount <=0) throw new IllegalArgumentException("금액은 양수여야 합니다.");
+        if (this.balance < amount) {
+            throw new IllegalStateException("잔액 부족");
+        }
+        this.balance -= amount;
+    }
 }
