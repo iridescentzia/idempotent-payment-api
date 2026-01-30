9f1ac8553f2645f9f913c2cc987c59ec64e3b696
iridescentzia <ziaunzip@naver.com>
2026-01-30 14:01:07 +0900

    feat:pointwallet리포지터리 작성

--- src/main/java/com/zia/payments/point/repository/PointWalletRepository.java
+++ src/main/java/com/zia/payments/point/repository/PointWalletRepository.java
@@ -1,4 +1,10 @@
 package com.zia.payments.point.repository;
 
-public class PointWalletRepository {
+import com.zia.payments.point.domain.PointWallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PointWalletRepository extends JpaRepository<PointWallet, Long> {
+    Optional<PointWallet> findByUserId(Long userId);
 }
